1c9ac8b9201807d922e3cd6709c8051b71e087a4
BunnyHatcher <martinengelhaus@gmx.de>
2022-08-26 18:04:21 +0200

    fixed overlap circle

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -9,12 +9,17 @@ public class PlayerMovement : MonoBehaviour
 
     [SerializeField] private float _jumpForce = 200f;
 
-    [SerializeField] private int _maxJump = 2;
+    [SerializeField] private byte _maxJump = 2;
 
     [SerializeField] private float _fallMultiplier = 3f;
 
     [Header("Related GameObjects")]
     [SerializeField] private GameObject _graphics; //to retrieve the graphics gameobject with our animations on it
+
+    [Header("Floor Detection")]
+    [SerializeField] private LayerMask _floorMask;
+    [SerializeField] private float _detectorRadius = 1.2f;
+    [SerializeField] private Vector2 _detectorPosition;
     
     
     private Rigidbody2D _rb2d;
@@ -41,6 +46,7 @@ public class PlayerMovement : MonoBehaviour
 
     void Update()
     {
+        //Horizontal movement
         _direction.x = Input.GetAxisRaw("Horizontal") * _moveSpeed;
 
         // character rotates by 180 degrees if he walks to the left (= less than zero velocity on the x axis)
@@ -65,19 +71,37 @@ public class PlayerMovement : MonoBehaviour
 
         _animator.SetFloat("MoveSpeedX", Mathf.Abs( _direction.x));//use Mathf.Abs to use negative direction values for movement
         _animator.SetFloat("MoveSpeedY", _rb2d.velocity.y);
+
+        // new method to detect if player touched floor
+
+        Vector2 finalPosition = new Vector2 (_detectorPosition.x + transform.position.x, _detectorPosition.y + transform.position.y);
         
+        Collider2D floorCollider = Physics2D.OverlapCircle(finalPosition, _detectorRadius, _floorMask);
+
+        if(floorCollider != null)
+        {
+            Debug.Log(floorCollider.tag);
+            Debug.Log("Touched the floor");
+            _nbJump = 0;
+
+        }
+        else
+        {
+            Debug.Log("Didn't touch the floor");
+        }
 
     }
 
     private void FixedUpdate()
     {
+        //Jumping physics
         _direction.y = _rb2d.velocity.y;
 
         if(_isJumping)
         {
             _direction.y = _jumpForce * Time.fixedDeltaTime;
             _isJumping = false; // to prevent spamming jump action
-            _nbJump++; // to keep count of number of jumps
+            _nbJump++; // increments number of jumps
         }
         //implementing gravity: the player will fall faster when in the air
         if (_rb2d.velocity.y < 0)
@@ -93,10 +117,21 @@ public class PlayerMovement : MonoBehaviour
             _animator.SetBool("isFalling", false);
         }
         
+        // Horizontal movement
         _rb2d.velocity = _direction;
 
     }
 
+    //Gizmo to visualize OverlapCircle
+    private void OnDrawGizmos()
+    {
+        Vector2 finalPosition = new Vector2(_detectorPosition.x + transform.position.x, _detectorPosition.y + transform.position.y);
+        Gizmos.DrawWireSphere(finalPosition, _detectorRadius);
+    }
+
+    /*    
+    //old method to detect if player touched the floor --> if the case, set _nbJump (=number of jumps) to zero
+    
     private void OnCollisionEnter2D(Collision2D collision)
     {
         if(collision.collider.CompareTag("Floor"))
@@ -104,4 +139,7 @@ public class PlayerMovement : MonoBehaviour
             _nbJump = 0;
         }
     }
+    */
+
+
 }
