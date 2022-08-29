f7204ff708010dff66a90aedfaf96974a9a0cb39
Martin Engelhaus <martinengelhaus@gmx.de>
2022-08-29 16:27:00 +0200

    smoothed camera movement via lerp

--- Assets/Scripts/MobilePlatform.cs
+++ Assets/Scripts/MobilePlatform.cs
@@ -8,15 +8,43 @@ public class MobilePlatform : MonoBehaviour
     [SerializeField] private Transform _endPoint;
     [SerializeField] private float _timeToReach = 5f;
 
+    
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        transform.position = _startPoint.position;
     }
 
     // Update is called once per frame
     void Update()
     {
         
+        Vector3 lerpPosition = Vector3.Lerp(_startPoint.position, _endPoint.position, _timerMovement / _timeToReach);
+
+        transform.position = lerpPosition;
+
+        if (_isForward)
+        {
+            _timerMovement += Time.deltaTime;
+
+            if( _timerMovement >= _timeToReach)
+            {
+                _isForward = false;
+            }
+        }
+
+        else
+        {
+            _timerMovement -= Time.deltaTime;
+
+            if(_timerMovement <= 0f)
+            {
+                _isForward = true;
+            }
+        }
     }
+
+    private float _timerMovement = 0f;
+    private bool _isForward = true;
 }
