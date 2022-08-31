4a218f219a333c362f3c386cbc4fa5fbb629bef9
Martin Engelhaus <martinengelhaus@gmx.de>
2022-08-31 12:40:17 +0200

    added ping pong and loop movement for platform

--- Assets/Scripts/WaypointPlatform.cs
+++ Assets/Scripts/WaypointPlatform.cs
@@ -8,18 +8,70 @@ public class WaypointPlatform : MonoBehaviour
 
     [SerializeField] private float _speed = 10;
 
+    [SerializeField] private float _reachTolerance = 0.1f;
+
     private void Start()
     {
-        _targetPlatform = 0;
+        
         transform.position = _waypoint[0].position;
+        _targetWayPointIndex = 1;
+
     }
 
     private void Update()
     {
-        Vector3 position = Vector3.MoveTowards(transform.position, _waypoint[_targetPlatform + 1].position, _speed * Time.deltaTime );
-        
-        transform.position = position;
-    }
+        Vector3 currentWaypointPosition = _waypoint[_targetWayPointIndex].position;
 
-    private int _targetPlatform;
+        Vector3 position = Vector3.MoveTowards(transform.position, currentWaypointPosition, _speed * Time.deltaTime);
+
+        transform.position = position;
+
+        // if platform gets close enough to the next waypont position,
+        // the method switches to the next waypoint position
+
+        if (Vector3.Distance(transform.position, currentWaypointPosition) <= _reachTolerance)
+        {
+
+
+
+            /* Loop
+          
+            _targetWayPointIndex++;
+
+            if (_targetWayPointIndex >= _waypoint.Length)
+            {
+             _targetWayPointIndex = 0;
+            }
+           */
+
+            // Ping-Pong
+            
+            if (_isForward)
+            {
+                _targetWayPointIndex++;
+
+                if (_targetWayPointIndex >= _waypoint.Length - 1)
+                {
+                    _isForward = false;
+                }
+            }
+
+            else
+            {
+                _targetWayPointIndex--;
+                if (_targetWayPointIndex <= 0)
+                {
+                    _isForward = true;
+                }
+            }
+
+        }
+
+    }
+       
+
+
+    private int _targetWayPointIndex;
+
+    private bool _isForward = true;
 }
